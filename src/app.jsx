2bebca0426793a7eb73c7836f4ff89321dac6297
JohnMurdo <98349101+JohnMurdo@users.noreply.github.com>
2026-02-24 11:16:09 -0700

    add navigate

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes, Navigate } from 'react-router-dom';
 import { Login } from './login/login.jsx';
 import { Read } from './read/read.jsx';
 import { Note } from './note/note.jsx';
@@ -24,6 +24,7 @@ export default function App() {
   <BrowserRouter>
   <div className="app bg-dark text-light"></div>
     <Routes>
+      <Route path='/' element={<Navigate to="/login" replace />} />
       <Route path='/login' element={<Login />} exact />
       <Route path='/read' element={<Read />} />
       <Route path='/note' element={<Note />} />
