0772ec3448770bb86fc835077362972581407238
JohnMurdo <98349101+JohnMurdo@users.noreply.github.com>
2026-02-24 22:05:15 -0700

    created authState and added some login functionality to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,47 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-import { BrowserRouter, NavLink, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
 import { Login } from './login/login.jsx';
 import { Read } from './read/read.jsx';
 import { Note } from './note/note.jsx';
+import { AuthState } from './login/authState.jsx';
 import { Friends } from './friends/friends.jsx';
 
 export default function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+  
+  useEffect(() => {
+      const html = document.documentElement;
+      const toggle = document.getElementById("toggle");
 
-    useEffect(() => {
-        const html = document.documentElement;
-        const toggle = document.getElementById("toggle");
-
-        if (toggle) {
-            toggle.onclick = () => {
-                html.dataset.theme = html.dataset.theme === "dark" ? "light" : "dark";
-            };
-        }
-    }, []);
+      if (toggle) {
+          toggle.onclick = () => {
+              html.dataset.theme = html.dataset.theme === "dark" ? "light" : "dark";
+          };
+      }
+  }, []);
 
   return (
   <BrowserRouter>
   <div className="app bg-dark text-light"></div>
     <Routes>
-      <Route path='/' element={<Navigate to="/login" replace />} />
-      <Route path='/login' element={<Login />} exact />
+      <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+            exact
+          />
       <Route path='/read' element={<Read />} />
       <Route path='/note' element={<Note />} />
       <Route path='/friends' element={<Friends />} />
